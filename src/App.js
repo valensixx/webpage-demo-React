fd33fe5015538e8c1c33cad1b2e7b2b459a999fc
valensixx <valensixx@gmail.com>
2023-04-24 20:27:10 +0300

    header.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/header/Header";
 
 /*Create Layout with components - Header, Menu, Footer 
 and Home page and use it in App component*/
@@ -6,7 +7,7 @@ and Home page and use it in App component*/
 export default function App(){
   return(
     <div>
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header(){
+    return(
+        <div>
+            <h1>This is the header</h1>
+        </div>
+    );
+}
