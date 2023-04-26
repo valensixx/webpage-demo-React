2f460b40ff0a973a605c48c7bbc4fb2725eeca0c
valensixx <valensixx@gmail.com>
2023-04-26 21:46:18 +0300

    Home.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Header from "./components/header/Header";
-import Menu from "./components/menu/Menu";
-import Footer from "./components/footer/Footer";
+
 
 /*Create Layout with components - Header, Menu, Footer 
 and Home page and use it in App component*/
@@ -9,9 +7,7 @@ and Home page and use it in App component*/
 export default function App(){
   return(
     <div>
-      <Header />
-      <Menu/>
-      <Footer/>
+      
     </div>
   );
 }
---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../home/Home.css";
+
+export default function Home(){
+    return(
+        <></>
+    );
+}
