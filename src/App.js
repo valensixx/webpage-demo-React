6b8b77bf53f0a77c8bca7a615c59c47f42006332
valensixx <valensixx@gmail.com>
2023-04-30 12:48:17 +0300

    menu css and fixing white screen error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-
-
+import Header from "./components/header/Header";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
 /*Create Layout with components - Header, Menu, Footer 
 and Home page and use it in App component*/
 
 export default function App(){
   return(
     <div>
-      
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
