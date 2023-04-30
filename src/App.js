19411a057cd116c363feddaf6c5cf95dc63aaaff
valensixx <valensixx@gmail.com>
2023-04-30 14:16:38 +0300

    task2 -carCard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Home from "./components/pages/home/Home";
 
-/*Create Layout with components - Header, Menu, Footer 
+/*TASK 1 Create Layout with components - Header, Menu, Footer 
 and Home page and use it in App component*/
+
+/*TASK 2 CarCard component with Wrapper(Model,brand, horse power and image of the car)
+create array of elements - {brand:"", model:"", image:"",horsePower:""} */
 
 export default function App(){
   return(
