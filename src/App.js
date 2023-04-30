b3633a92122d184cfa7f3ed65daa94f52d2c8886
valensixx <valensixx@gmail.com>
2023-04-30 14:09:10 +0300

    layout components done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./components/pages/home/Home";
+
 /*Create Layout with components - Header, Menu, Footer 
 and Home page and use it in App component*/
 
---
+++ src/components/pages/about/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import withLayout from "../../layouts/withLayout";
+
+function About() {
+    return (
+        <main>
+            <h1>Hello from about!</h1>
+        </main>
+    );
+}
+
+export default withLayout(About);
