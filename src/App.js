1a6a03043eded85b5079431e413e1881e77315ce
valensixx <valensixx@gmail.com>
2023-04-30 17:17:52 +0300

    Task 4 - FocusBtn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./components/pages/home/Home";
 import Quotes from "./components/Quotes";
+import FocusBtn from "./components/FocusBtn";
 
 /*TASK 1 
 Create Layout with components - Header, Menu, Footer 
@@ -16,11 +17,18 @@ Create component Quotes, generating random quote from array in every 10 seconds.
 The quotes are taken form array made my me.
 I type the code in Quotes component */
 
+/*TASK 4 
+Create 2 btn and 2 text fields.
+On click - button1 will focus the text field1
+On click - button2 will focus the text field2
+*/
+
 export default function App() {
   return (
     <div>
       <Home />
       <Quotes />
+      <FocusBtn />
     </div>
   );
 }
---
+++ src/components/FocusBtn.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/*TASK 4 
+Create 2 btn and 2 text fields.
+On click - button1 will focus the text field1
+On click - button2 will focus the text field2
+*/
+
+export default function FocusBtn(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
