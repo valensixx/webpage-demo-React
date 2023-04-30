d280956c26956f42441efe03e2962cb2ed398dcc
valensixx <valensixx@gmail.com>
2023-04-30 15:16:20 +0300

    trying to fix images not showing...

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -7,7 +7,10 @@ import CarCard from "../../card/CarCard";
 /*TASK 2 CarCard component with Wrapper(Model,brand, horse power and image of the car)
 create array of elements - {brand:"", model:"", image:"",horsePower:""} */
 
+const imagePath = "/images";
+
 function Home() {
+    
     const cars = [
         { brand: "Mercedes", model: "ML", image: "/images/1.jpg", horsePower: "250hp" },
         { brand: "Audi", model: "A8", image: "/images/2.jpg", horsePower: "200hp" },
