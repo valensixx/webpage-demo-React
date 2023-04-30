539948f5581637efde30137683b2b5e509a95063
valensixx <valensixx@gmail.com>
2023-04-30 15:08:10 +0300

    changes

---
+++ src/components/card/CarCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CarCard.css";
+
+export default function CarCard(props) {
+    return (
+        <div className="card">
+            <img src={props.img}/>
+            <div className="card-content">
+                <h2>{props.brand} - {props.model}</h2>
+                <p>{props.horsePower} Horse Power</p>
+                <button>Learn more!</button>
+            </div>
+        </div>
+    );
+}
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,20 +1,21 @@
 import React from "react";
 import withLayout from "../../layouts/withLayout";
 import "../home/Home.css";
+import CarCard from "../../card/CarCard";
 
 
 /*TASK 2 CarCard component with Wrapper(Model,brand, horse power and image of the car)
 create array of elements - {brand:"", model:"", image:"",horsePower:""} */
 
 function Home() {
-    const cars=[
-        {brand:"Mercedes", model:"ML", image:"/images/mb-ml.jpg", horsePower:"250hp"},
-        {brand:"Audi", model:"A8", image:"/images/audiA8.jpg", horsePower:"200hp"},
-        {brand:"BMW", model:"320", image:"/images/bmw-320.jpg", horsePower:"150hp"}
+    const cars = [
+        { brand: "Mercedes", model: "ML", image: "/images/1.jpg", horsePower: "250hp" },
+        { brand: "Audi", model: "A8", image: "/images/2.jpg", horsePower: "200hp" },
+        { brand: "BMW", model: "320", image: "/images/3.jpg", horsePower: "150hp" }
     ];
     return (
         <div>
-           
+            {cars.map(c => <CarCard {...c} />)}
         </div>
     );
 }
