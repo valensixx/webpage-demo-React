2f77b9ac124436e097b74c3eeeeef7bfff946ae4
valensixx <valensixx@gmail.com>
2023-04-30 17:24:03 +0300

    changes

--- src/components/FocusBtn.js
+++ src/components/FocusBtn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 
 /*TASK 4 
 Create 2 btn and 2 text fields.
@@ -9,7 +9,14 @@ On click - button2 will focus the text field2
 export default function FocusBtn(){
     return(
         <div>
-            
+            <div>
+                <input type="text" />
+                <button>Click button 1</button>
+            </div>
+            <div>
+                <input type="text" />
+                <button>Click button 2</button>
+            </div>
         </div>
     );
 }
