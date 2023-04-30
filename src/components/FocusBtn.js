c4d698d3cb09a1d88ec323bac5970888b9a602f0
valensixx <valensixx@gmail.com>
2023-04-30 17:34:38 +0300

    task 4 done!

--- src/components/FocusBtn.js
+++ src/components/FocusBtn.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 
 /*TASK 4 
 Create 2 btn and 2 text fields.
@@ -6,16 +6,23 @@ On click - button1 will focus the text field1
 On click - button2 will focus the text field2
 */
 
-export default function FocusBtn(){
-    return(
+export default function FocusBtn() {
+    const refOne = useRef(null);
+    const refTwo = useRef(null);
+
+    return (
         <div>
             <div>
-                <input type="text" />
-                <button>Click button 1</button>
+                <input type="text" ref={refOne} />
+                <button onClick={() =>
+                    refOne.current.focus()
+                }>Click button 1</button>
             </div>
             <div>
-                <input type="text" />
-                <button>Click button 2</button>
+                <input type="text" ref={refTwo} />
+                <button onClick={() =>
+                    refTwo.current.focus()
+                }>Click button 2</button>
             </div>
         </div>
     );
